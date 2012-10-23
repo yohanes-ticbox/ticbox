824f491c9c4bbf29465b0a7a9664aabb9adf379d
Firman Agustian <firman@ticbox.co>
2012-10-23 23:01:14 +0700

    remove some file

--- grails-app/controllers/com/chicow/ticbox/RespondentController.groovy
+++
@@ -1,7 +0,0 @@
-package com.chicow.ticbox
-
-class RespondentController {
-	static scaffold = Respondent
-	
-	def form = {}
-}
--- grails-app/controllers/com/chicow/ticbox/UserController.groovy
+++
@@ -1,7 +0,0 @@
-package com.chicow.ticbox
-
-class UserController {
-	static scaffold = User
-	
-	def form = {}
-}
